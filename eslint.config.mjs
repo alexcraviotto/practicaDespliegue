8630d798c42ee640db5ded9a80a1e8662231f558
Antonio Ordóñez <aom387@inlumine.ual.es>
2025-04-08 16:12:13 +0200

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  // Ignorar archivos específicos globalmente
+  [globalIgnores(["**/test.js", "**/buzz.min.js", "**/jquery-3.1.1.min.js"])],
+
+  // Configuración para todos los archivos JavaScript y TypeScript
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      globals: globals.browser, // Define los globals del entorno del navegador
+    },
+  },
+
+  // Configuración recomendada para TypeScript
+  tseslint.configs.recommended,
+
+  // Reglas personalizadas
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off", // Deshabilita la regla completamente
+    },
+  },
+]);
