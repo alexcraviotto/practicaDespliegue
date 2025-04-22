fba3a22ba9b3327db41f815d5ad816ee8f4c863f
alexcraviotto <alexcraviotto@pm.me>
2025-04-22 17:24:12 +0200

    feat: base

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,7 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
-import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
 
 export default defineConfig([
-  // Ignorar archivos específicos globalmente
-  [globalIgnores(["**/test.js", "**/buzz.min.js", "**/jquery-3.1.1.min.js"])],
-
-  // Configuración para todos los archivos JavaScript y TypeScript
-  {
-    files: ["**/*.{js,mjs,cjs,ts}"],
-    languageOptions: {
-      globals: globals.browser, // Define los globals del entorno del navegador
-    },
-  },
-
-  // Configuración recomendada para TypeScript
-  tseslint.configs.recommended,
-
-  // Reglas personalizadas
-  {
-    rules: {
-      "@typescript-eslint/no-unused-vars": "off", // Deshabilita la regla completamente
-    },
-  },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
 ]);
---
+++ test/bird.test.js
@@ -0,0 +1,63 @@
+const $ = require("jquery");
+require("jsdom-global");
+
+describe("changeColor function", () => {
+  let bird;
+  let inmune;
+
+  beforeEach(() => {
+    bird = {
+      width: 30,
+      height: 20,
+      positionX: 60,
+      positionY: 210,
+      velocityY: 6,
+      velocityReset: 7.2,
+      changeColor: function () {
+        if (inmune.have) {
+          $(".bird").css({
+            "background-image": "url(images/bird-invincible.png)",
+          });
+        } else {
+          $(".bird").css({ "background-image": "url(images/bird.png)" });
+        }
+      },
+    };
+
+    inmune = {
+      have: false,
+      time: 300,
+      duration: function () {
+        this.time--;
+        if (!this.time && this.have) {
+          this.have = false;
+        }
+      },
+    };
+
+    $("body").append('<div class="bird"></div>');
+  });
+
+  it("should change the background image when inmune.have is true", () => {
+    inmune.have = true;
+    bird.changeColor();
+    const backgroundImage = $(".bird").css("background-image");
+    expect(backgroundImage).toBe("url(images/bird-invincible.png)");
+  });
+
+  it("should change the background image when inmune.have is false", () => {
+    inmune.have = false;
+    bird.changeColor();
+    const backgroundImage = $(".bird").css("background-image");
+    expect(backgroundImage).toBe("url(images/bird.png)");
+  });
+
+  // test que falla a propósito
+  it("should fail", () => {
+    expect(true).toBe(false);
+  });
+
+  afterEach(() => {
+    $(".bird").remove();
+  });
+}); 
